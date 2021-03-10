fb4b2b359a5104c92ca46049dea668016b519685
Rey Eduard Torres <2047584@aup.edu.ph>
2021-03-10 20:15:42 +0800

    add components such as display, header, tips, and the main calc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainCalculatorComponent } from './main-calculator/main-calculator.component';
+import { TipsComponent } from './tips/tips.component';
+import { HeaderComponent } from './header/header.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCalculatorComponent,
+    TipsComponent,
+    HeaderComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule
